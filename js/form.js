1c0284863d5e7542d58bb7849bc0bf909407c248
TejeshCodes <63721912+TejeshCodes@users.noreply.github.com>
2020-05-28 20:12:36 +0530

    Add files via upload

---
+++ js/form.js
@@ -0,0 +1,42 @@
+class Form{
+    constructor(){
+        this.input = createInput("Name");
+        this.input.position(displayWidth/2 - 40, displayHeight/2 -80);
+
+        this.button = createButton("PLAY");
+        this.button.position(displayWidth/2 + 30, displayHeight/2);
+        
+        this.greeting = createElement("h3");
+        this.greeting.position(displayWidth/2 - 70, displayHeight/4);
+    }
+    display(){
+        var title = createElement("h2");
+        title.html("Car Racing Game");
+        title.position(displayWidth/2 - 50, 50);
+
+        
+
+        this.button.mousePressed(() => {
+            this.button.hide();
+            this.input.hide();
+
+            player.name = this.input.value();
+
+            playerCount = playerCount + 1;
+            player.index = playerCount;
+
+            player.update();
+            player.updateCount(playerCount);
+
+            this.greeting.html("Hello "+ player.name);
+
+        })
+
+    }
+
+    hide(){
+        this.button.hide();
+        this.input.hide();
+        this.greeting.hide();
+    }
+}
